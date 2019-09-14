f977424afc332776effb56638a44fe8db998e84c
shadow-jafar <shadow.jafar@icloud.com>
2019-09-14 10:35:51 +0400

    test final

---
+++ SpotlightLyrics/LyricsView/SpotlightLyrics.h
@@ -0,0 +1,24 @@
+//
+//  SpotlightLyrics.h
+//  SpotlightLyrics
+//
+//  Created by Jafar Khan on 9/14/19.
+//  Copyright © 2019 Scott Rong. All rights reserved.
+//
+
+#ifndef SpotlightLyrics_h
+#define SpotlightLyrics_h
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NVMDummy.
+FOUNDATION_EXPORT double SpotlightLyricsVersionNumber;
+
+//! Project version string for NVMDummy.
+FOUNDATION_EXPORT const unsigned char SpotlightLyricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NVMDummy/PublicHeader.h>
+#import <SpotlightLyrics/KaraokeLabel.h>
+#import <SpotlightLyrics/VTXKaraokeLyricView.h>
+
+#endif /* SpotlightLyrics_h */
